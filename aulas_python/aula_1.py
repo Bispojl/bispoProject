7971bfc857870596a5c324b42456572431ccb629
macOS <macos@iMac-de-macOS.local>
2024-02-26 16:19:52 -0300

    adicionando aula1

---
+++ aulas_python/aula_1.py
@@ -0,0 +1,8 @@
+"""
+Python -> Linguagem de programação
+Tipo de tipagem -> Dinamica e Forte
+str -> string -> texto 
+"""
+
+print(123) # printa 123
+print(10, 20) #printa 10, 20 de forma que separe os argumentos dentro dos parenteses
