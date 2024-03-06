4e9deefe735d397111aa95e5f99c8046d4700012
macOS <macos@iMac-de-macOS.local>
2024-03-05 23:27:12 -0300

    atualizando aulas

--- aulas_python/aula_0.py
+++ aulas_python/aula_0.py
@@ -1,1 +1,2 @@
+#https://docs.python.org/pt-br/3/library/stdtypes.html
 print('hello world')
--- aulas_python/aula_20.py
+++ aulas_python/aula_20.py
@@ -9,3 +9,16 @@ id = identidade
 #o python consegue identificar o valor literal, e acaba retornando o mesmo id do espaço da. 
 v1 = 'a' 
 v2 = 'a'
+
+
+#OBS : IMUTAVEIS QUE VIMOS: str, int, float e bool
+
+string = "João Bispo"
+variavel_1 = f'{string[:5]}HELLO{string[4:]}' 
+"""
+como mencionado acima, as strings são imutaveis, caso vc queira muda-las ou manipular o seu valor de alguma forma,
+voce pode utilizar o .format, conforme o exeplo acima. onde ele pega os valores do inicio até o indice 5, acrecenta 
+o valor HELLO e concatena com tudo que vier a partir do indice 4 
+"""
+
+print(variavel_1)
